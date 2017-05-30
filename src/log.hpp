f8c6c613494ef569d2fbcd5124a54a98809a68a2
gyf1214 <gyf1214@gmail.com>
2017-05-30 10:12:59 +0800

    new log.hpp

--- src/log.hpp
+++ src/log.hpp
@@ -36,18 +36,30 @@ struct LogStream {
 #define DEFAULT_LOG std::cerr
 #endif
 
+#define EMPTY       LOG_TO(DEFAULT_LOG, false, false)
 #define LOG         LOG_TO(DEFAULT_LOG, true, false)
-#define LOG_IF(x)   ({ bool __log = (x) ? true : false; LOG_TO(DEFAULT_LOG, __log, false); })
 #define FAIL        LOG_TO(DEFAULT_LOG, true, true)
 
+#ifndef NDEBUG
+#define LOG_IF(x)   ({ bool __log = (x) ? true : false; LOG_TO(DEFAULT_LOG, __log, false); })
+#else
+#define LOG_IF(x)   EMPTY
+#endif
+
+#ifndef NCHECK
 #define CHECK(x)    ({ bool __fail = (x) ? false : true; LOG_TO(DEFAULT_LOG, __fail, __fail); })
+#else
+#define CHECK(x)    EMPTY
+#endif
+
 #define CHECK_EQ(x, y) CHECK((x) == (y))
 #define CHECK_NQ(x, y) CHECK((x) != (y))
+#define CHECK_NORM(x)  CHECK_EQ(cmp(glm::length(x), 1.0f), 0)
 
 #ifndef NDEBUG
-#define CHECK_GL()  ({ GLenum err = glGetError(); CHECK_EQ(err, GL_NO_ERROR) << "gl error: " << err; })
+#define CHECK_GL      ({ GLenum err = glGetError(); CHECK_EQ(err, GL_NO_ERROR) << "gl error: " << err; })
 #else
-#define CHECK_GL()
+#define CHECK_GL
 #endif
 
 #endif
